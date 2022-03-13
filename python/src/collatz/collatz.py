a7279d242f5d3648aeac217be5cf5831f4ec8503
Skenvy <17214791+Skenvy@users.noreply.github.com>
2022-03-13 22:51:37 +1100

    Finish parameterised function descriptions

--- python/src/collatz/collatz.py
+++ python/src/collatz/collatz.py
@@ -38,10 +38,14 @@ def parameterised_hailstone_sequence(initial_value:int, P:int = 2, a:int = 3,
                                      b:int=1, max_total_stopping_time:int=1000,
                                      terminate_at_stopping_time:bool=False):
     """
-    *
+    Returns a list of successive values obtained by iterating the Collatz-esque
+    function, until either 1 is reached, or the total amount of iterations
+    exceeds max_total_stopping_time, unless terminate_at_stopping_time is True,
+    which will terminate the hailstone at the "stopping time" value, i.e. the
+    first value less than the initial value.
 
     Args:
-        initial_value (int):
+        initial_value (int): The value to begin the hailstone sequence from.
     
     Kwargs:
         P (int): Modulus used to devide n, iff n is equivalent to (0 mod P)
@@ -62,10 +66,13 @@ def parameterised_stopping_time(initial_value:int, P:int = 2, a:int = 3,
                                 b:int=1, max_stopping_time:int=1000,
                                 total_stopping_time:bool=False):
     """
-    *
+    Returns the stopping time, the amount of iterations required to reach a
+    value less than the initial value, or None if max_stopping_time is exceeded.
+    Alternatively, if total_stopping_time is True, then it will instead count
+    the amount of iterations to reach 1.
 
     Args:
-        initial_value (int):
+        initial_value (int): The value for which to find the stopping time.
     
     Kwargs:
         P (int): Modulus used to devide n, iff n is equivalent to (0 mod P)
@@ -86,10 +93,11 @@ def parameterised_stopping_time(initial_value:int, P:int = 2, a:int = 3,
 
 def parameterised_reverse_function(n:int, P:int = 2, a:int = 3, b:int=1):
     """
-    *
+    Returns the output of a single application of a Collatz-esque reverse
+    function.
 
     Args:
-        n (int):
+        n (int): The value on which to perform the reverse Collatz function
     
     Kwargs:
         P (int): Modulus used to devide n, iff n is equivalent to (0 mod P)
@@ -103,12 +111,18 @@ def parameterised_reverse_function(n:int, P:int = 2, a:int = 3, b:int=1):
 def parameterised_tree_graph(initial_value:int, max_orbit_distance:int,
                              P:int = 2, a:int = 3, b:int=1):
     """
-    *
+    Returns nested dictionaries that model the directed tree graph up to a
+    maximum nesting of max_orbit_distance, with the initial_value as the root.
 
     Args:
-        initial_value (int):
+        initial_value (int): The root value of the directed tree graph.
         max_orbit_distance (int): Maximum amount of times to iterate the reverse
-            function. There is no natural 
+            function. There is no natural termination to populating the tree
+            graph, equivalent to the termination of hailstone sequences or
+            stopping time attempts, so this is not an optional argument like
+            max_stopping_time / max_total_stopping_time, as it is the intended
+            target of orbits to obtain, rather than a limit to avoid uncapped
+            computation.
     
     Kwargs:
         P (int): Modulus used to devide n, iff n is equivalent to (0 mod P)
