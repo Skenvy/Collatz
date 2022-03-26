e9e16a94f48c047c25de2f92c19df9f70a93a35f
Skenvy <17214791+Skenvy@users.noreply.github.com>
2022-03-27 04:53:10 +1100

    Expanded makes, initiate docs, add log for how to add hosting later

---
+++ julia/docs/make.jl
@@ -0,0 +1,3 @@
+# push!(LOAD_PATH,"../src/")
+using Documenter, Collatz
+makedocs(sitename="Collatz", modules=[Collatz])
--- julia/src/Collatz.jl
+++ julia/src/Collatz.jl
@@ -1,5 +1,8 @@
 module Collatz
 
+"""
+The default/example greeting function.
+"""
 greet() = print("Hello World!")
 
 end # module
