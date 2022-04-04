a3be82a19f6d3442c1aa72ca93e015315562b425
Skenvy <17214791+Skenvy@users.noreply.github.com>
2022-04-04 15:05:44 +1000

    Add the python tests as comments to run tests and add precompile to the make test

--- julia/src/Collatz.jl
+++ julia/src/Collatz.jl
@@ -7,8 +7,8 @@ tree-graph.
 """
 module Collatz
 
-# from typing import Optional, Set
-# from math import inf as infinity
+#TODO: from typing import Optional, Set
+#TODO: from math import inf as infinity
 
 
 const _KNOWN_CYCLES = [[1, 4, 2], [-1, -2], [-5, -14, -7, -20, -10], [-17,-50,-25,-74,-37,-110,-55,-164,-82,-41,-122,-61,-182,-91,-272,-136,-68,-34]]
@@ -31,7 +31,7 @@ Cycle Control: Descriptive flags to indicate when some event occurs in the
 hailstone sequences, when set to verbose, or stopping time check.
 """
 module _CC
-# Access as string(ABC::CC) or just string(ABC)
+#TODO: Access as string(ABC::CC) or just string(ABC)
 @enum CC begin
     STOPPING_TIME
     TOTAL_STOPPING_TIME
@@ -93,93 +93,98 @@ function _function(n::Integer; P::Integer=2, a::Integer=3, b::Integer=1)
     end
 end
 
-# def reverse_function(n::Integer, P::Integer=2, a::Integer=3, b::Integer=1):
-    """
-    Returns the output of a single application of a Collatz-esque reverse
-    function.
 
-    Args:
-        n (int): The value on which to perform the reverse Collatz function
-    
-    Kwargs:
-        P (int): Modulus used to devide n, iff n is equivalent to (0 mod P)
-            Default is 2.
-        a (int): Factor by which to multiply n. Default is 3.
-        b (int): Value to add to the scaled value of n. Default is 1.
-    """
-#     __assert_sane_parameterisation(P,a,b)
-#     # Every input can be reversed as the result of "n/P" division, which yields
-#     # "Pn"... {f(n) = an + b}≡{(f(n) - b)/a = n} ~ if n was such that the
-#     # muliplication step was taken instead of the division by the modulus, then
-#     # (f(n) - b)/a) must be an integer that is not in (0 mod P). Because we're
-#     # not placing restrictions on the parameters yet, although there is a better
-#     # way of shortcutting this for the default variables, we need to always
-#     # attempt (f(n) - b)/a)
+"""
+Returns the output of a single application of a Collatz-esque reverse
+function.
+
+Args:
+    n (int): The value on which to perform the reverse Collatz function
+
+Kwargs:
+    P (int): Modulus used to devide n, iff n is equivalent to (0 mod P)
+        Default is 2.
+    a (int): Factor by which to multiply n. Default is 3.
+    b (int): Value to add to the scaled value of n. Default is 1.
+"""
+function reverse_function(n::Integer; P::Integer=2, a::Integer=3, b::Integer=1)
+    __assert_sane_parameterisation(P,a,b)
+    # Every input can be reversed as the result of "n/P" division, which yields
+    # "Pn"... {f(n) = an + b}≡{(f(n) - b)/a = n} ~ if n was such that the
+    # muliplication step was taken instead of the division by the modulus, then
+    # (f(n) - b)/a) must be an integer that is not in (0 mod P). Because we're
+    # not placing restrictions on the parameters yet, although there is a better
+    # way of shortcutting this for the default variables, we need to always
+    # attempt (f(n) - b)/a)
+#TODO:
 #     pre_values = [P*n]
 #     if (n-b)%a == 0 and (n-b)%(P*a) != 0:
 #         pre_values += [(n-b)//a]
 #         pre_values.sort()
 #     return pre_values
+    return 1
+end
 
 
-# def __initial_value_outside_verified_range(x::Integer):
-    """
-    Checks if the initial value is greater than __VERIFIED_MAXIMUM or less than
-    __VERIFIED_MINIMUM. Only intended for the default parameterisation.
+"""
+Checks if the initial value is greater than __VERIFIED_MAXIMUM or less than
+__VERIFIED_MINIMUM. Only intended for the default parameterisation.
 
-    Args:
-        x (int): The initial value to check if it is within range or not.
-    """
-#     return (__VERIFIED_MAXIMUM < x) or (x < __VERIFIED_MINIMUM)
-
-
-# def __stopping_time_terminus(n::Integer, total_stop:bool):
-    """
-    Provides the appropriate lambda to use to check if iterations on an initial
-    value have reached either the stopping time, or total stopping time.
-
-    Args:
-        n (int): The initial value to confirm against a stopping time check.
-        total_stop (bool): If false, the lambda will confirm that iterations
-            of n have reached the oriented stopping time to reach a value closer
-            to 0. If true, the lambda will simply check equality to 1.
-    """
-#     return (lambda x: x == 1) if total_stop else ((lambda x: x < n and x > 0)
-#                                    if n >= 0 else (lambda x: x > n and x < 0))
+Args:
+    x (int): The initial value to check if it is within range or not.
+"""
+function __initial_value_outside_verified_range(x::Integer) #TODO:
+    # return (__VERIFIED_MAXIMUM < x) or (x < __VERIFIED_MINIMUM)
+    return 1
+end
 
 
-# def hailstone_sequence(initial_value::Integer, P::Integer=2, a::Integer=3,
-#                        b::Integer=1, max_total_stopping_time::Integer=1000,
-#                        total_stopping_time:bool=True, verbose:bool=True):
-    """
-    Returns a list of successive values obtained by iterating a Collatz-esque
-    function, until either 1 is reached, or the total amount of iterations
-    exceeds max_total_stopping_time, unless total_stopping_time is False,
-    which will terminate the hailstone at the "stopping time" value, i.e. the
-    first value less than the initial value. While the sequence has the
-    capability to determine that it has encountered a cycle, the cycle from "1"
-    wont be attempted or reported as part of a cycle, regardless of default or
-    custom parameterisation, as "1" is considered a "total stop".
+"""
+Provides the appropriate lambda to use to check if iterations on an initial
+value have reached either the stopping time, or total stopping time.
 
-    Args:
-        initial_value (int): The value to begin the hailstone sequence from.
+Args:
+    n (int): The initial value to confirm against a stopping time check.
+    total_stop (bool): If false, the lambda will confirm that iterations
+        of n have reached the oriented stopping time to reach a value closer
+        to 0. If true, the lambda will simply check equality to 1.
+"""
+function __stopping_time_terminus(n::Integer, total_stop::Bool) #TODO:
+    # return (lambda x: x == 1) if total_stop else ((lambda x: x < n and x > 0) if n >= 0 else (lambda x: x > n and x < 0))
+    return 1
+end
+
+
+"""
+Returns a list of successive values obtained by iterating a Collatz-esque
+function, until either 1 is reached, or the total amount of iterations
+exceeds max_total_stopping_time, unless total_stopping_time is False,
+which will terminate the hailstone at the "stopping time" value, i.e. the
+first value less than the initial value. While the sequence has the
+capability to determine that it has encountered a cycle, the cycle from "1"
+wont be attempted or reported as part of a cycle, regardless of default or
+custom parameterisation, as "1" is considered a "total stop".
+
+Args:
+    initial_value (int): The value to begin the hailstone sequence from.
     
-    Kwargs:
-        P (int): Modulus used to devide n, iff n is equivalent to (0 mod P)
-            Default is 2.
-        a (int): Factor by which to multiply n. Default is 3.
-        b (int): Value to add to the scaled value of n. Default is 1.
-        max_total_stopping_time (int): Maximum amount of times to iterate the
-            function, if 1 is not reached. Default is 1000.
-        total_stopping_time (bool): Whether or not to execute until the "total"
-            stopping time (number of iterations to obtain 1) rather than the
-            regular stopping time (number of iterations to reach a value less
-            than the initial value). Default is True.
-        verbose (bool): If set to verbose, the hailstone sequence will include
-            control string sequences to provide information about how the
-            sequence terminated, whether by reaching a stopping time or entering
-            a cycle. Default is True.
-    """
+Kwargs:
+    P (int): Modulus used to devide n, iff n is equivalent to (0 mod P)
+        Default is 2.
+    a (int): Factor by which to multiply n. Default is 3.
+    b (int): Value to add to the scaled value of n. Default is 1.
+    max_total_stopping_time (int): Maximum amount of times to iterate the
+        function, if 1 is not reached. Default is 1000.
+    total_stopping_time (bool): Whether or not to execute until the "total"
+        stopping time (number of iterations to obtain 1) rather than the
+        regular stopping time (number of iterations to reach a value less
+        than the initial value). Default is True.
+    verbose (bool): If set to verbose, the hailstone sequence will include
+        control string sequences to provide information about how the
+        sequence terminated, whether by reaching a stopping time or entering
+        a cycle. Default is True.
+"""
+function hailstone_sequence(initial_value::Integer; P::Integer=2, a::Integer=3, b::Integer=1, max_total_stopping_time::Integer=1000, total_stopping_time::Bool=True, verbose::Bool=True) #TODO:
 #     # Call out the function before any magic returns to trap bad values.
 #     _ = function(initial_value,P=P,a=a,b=b)
 #     # 0 is always an immediate stop.
@@ -225,84 +230,85 @@ end
 #         if verbose:
 #             hailstone += [[_CC.MAX_STOP_OOB.value, _max_total_stopping_time]]
 #     return hailstone
+    return 1
+end
 
 
-# def stopping_time(initial_value::Integer, P::Integer=2, a::Integer=3,
-#                   b::Integer=1, max_stopping_time::Integer=1000,
-#                   total_stopping_time:bool=False):
-    """
-    Returns the stopping time, the amount of iterations required to reach a
-    value less than the initial value, or None if max_stopping_time is exceeded.
-    Alternatively, if total_stopping_time is True, then it will instead count
-    the amount of iterations to reach 1. If the sequence does not stop, but
-    instead ends in a cycle, the result will be (math.inf). If (P,a,b) are such
-    that it is possible to get stuck on zero, the result will be the negative of
-    what would otherwise be the "total stopping time" to reach 1, where 0 is
-    considered a "total stop" that should not occur as it does form a cycle of
-    length 1.
+"""
+Returns the stopping time, the amount of iterations required to reach a
+value less than the initial value, or None if max_stopping_time is exceeded.
+Alternatively, if total_stopping_time is True, then it will instead count
+the amount of iterations to reach 1. If the sequence does not stop, but
+instead ends in a cycle, the result will be (math.inf). If (P,a,b) are such
+that it is possible to get stuck on zero, the result will be the negative of
+what would otherwise be the "total stopping time" to reach 1, where 0 is
+considered a "total stop" that should not occur as it does form a cycle of
+length 1.
 
-    Args:
-        initial_value (int): The value for which to find the stopping time.
+Args:
+    initial_value (int): The value for which to find the stopping time.
     
-    Kwargs:
-        P (int): Modulus used to devide n, iff n is equivalent to (0 mod P)
-            Default is 2.
-        a (int): Factor by which to multiply n. Default is 3.
-        b (int): Value to add to the scaled value of n. Default is 1.
-        max_stopping_time (int): Maximum amount of times to iterate the
-            function, if the stopping time is not reached. IF the
-            max_stopping_time is reached, the function will return None.
-            Default is 1000.
-        total_stopping_time (bool): Whether or not to execute until the "total"
-            stopping time (number of iterations to obtain 1) rather than the
-            regular stopping time (number of iterations to reach a value less
-            than the initial value). Default is False.
-    """
-#     # The information is contained in the verbose form of a hailstone sequence.
-#     # Although the "max_~_time" for hailstones is name for "total stopping" time
-#     # and the "max_~_time" for this "stopping time" function is _not_ "total",
-#     # they are handled the same way, as the default for "total_stopping_time"
-#     # for hailstones is true, but for this, is false. Thus the naming difference
+Kwargs:
+    P (int): Modulus used to devide n, iff n is equivalent to (0 mod P)
+        Default is 2.
+    a (int): Factor by which to multiply n. Default is 3.
+    b (int): Value to add to the scaled value of n. Default is 1.
+    max_stopping_time (int): Maximum amount of times to iterate the
+        function, if the stopping time is not reached. IF the
+        max_stopping_time is reached, the function will return None.
+        Default is 1000.
+    total_stopping_time (bool): Whether or not to execute until the "total"
+        stopping time (number of iterations to obtain 1) rather than the
+        regular stopping time (number of iterations to reach a value less
+        than the initial value). Default is False.
+"""
+function stopping_time(initial_value::Integer; P::Integer=2, a::Integer=3, b::Integer=1, max_stopping_time::Integer=1000, total_stopping_time::Bool=False) #TODO:
+    # The information is contained in the verbose form of a hailstone sequence.
+    # Although the "max_~_time" for hailstones is name for "total stopping" time
+    # and the "max_~_time" for this "stopping time" function is _not_ "total",
+    # they are handled the same way, as the default for "total_stopping_time"
+    # for hailstones is true, but for this, is false. Thus the naming difference
 #     end_msg = hailstone_sequence(initial_value, P=P, a=a, b=b, verbose=True,
 #                                  max_total_stopping_time=max_stopping_time,
 #                                  total_stopping_time=total_stopping_time)[-1]
-#     # For total/regular/zero stopping time, the value is already the same as
-#     # that present, for cycles we report infinity instead of the cycle length,
-#     # and for max stop out of bounds, we report None instead of the max stop cap
+    # For total/regular/zero stopping time, the value is already the same as
+    # that present, for cycles we report infinity instead of the cycle length,
+    # and for max stop out of bounds, we report None instead of the max stop cap
 #     return {_CC.TOTAL_STOPPING_TIME.value: end_msg[1],
 #             _CC.STOPPING_TIME.value: end_msg[1],
 #             _CC.CYCLE_LENGTH.value: infinity,
 #             _CC.ZERO_STOP.value: end_msg[1],
 #             _CC.MAX_STOP_OOB.value: None,
 #             }.get(end_msg[0], None)
+    return 1
+end
 
 
-# def tree_graph(initial_value::Integer, max_orbit_distance::Integer, P::Integer=2, a::Integer=3,
-#                b::Integer=1, __cycle_prevention:Optional[Set[int]]=None):
-    """
-    Returns nested dictionaries that model the directed tree graph up to a
-    maximum nesting of max_orbit_distance, with the initial_value as the root.
+"""
+Returns nested dictionaries that model the directed tree graph up to a
+maximum nesting of max_orbit_distance, with the initial_value as the root.
 
-    Args:
-        initial_value (int): The root value of the directed tree graph.
-        max_orbit_distance (int): Maximum amount of times to iterate the reverse
-            function. There is no natural termination to populating the tree
-            graph, equivalent to the termination of hailstone sequences or
-            stopping time attempts, so this is not an optional argument like
-            max_stopping_time / max_total_stopping_time, as it is the intended
-            target of orbits to obtain, rather than a limit to avoid uncapped
-            computation.
-    
-    Kwargs:
-        P (int): Modulus used to devide n, iff n is equivalent to (0 mod P)
-            Default is 2.
-        a (int): Factor by which to multiply n. Default is 3.
-        b (int): Value to add to the scaled value of n. Default is 1.
-    
-    Internal Kwargs:
-        __cycle_prevention (set[int]): Used to prevent cycles from precipitating
-            by keeping track of all values added across previous nest depths.
-    """
+Args:
+    initial_value (int): The root value of the directed tree graph.
+    max_orbit_distance (int): Maximum amount of times to iterate the reverse
+        function. There is no natural termination to populating the tree
+        graph, equivalent to the termination of hailstone sequences or
+        stopping time attempts, so this is not an optional argument like
+        max_stopping_time / max_total_stopping_time, as it is the intended
+        target of orbits to obtain, rather than a limit to avoid uncapped
+        computation.
+
+Kwargs:
+    P (int): Modulus used to devide n, iff n is equivalent to (0 mod P)
+        Default is 2.
+    a (int): Factor by which to multiply n. Default is 3.
+    b (int): Value to add to the scaled value of n. Default is 1.
+
+Internal Kwargs:
+    __cycle_prevention (set[int]): Used to prevent cycles from precipitating
+        by keeping track of all values added across previous nest depths.
+"""
+function tree_graph(initial_value::Integer, max_orbit_distance::Integer; P::Integer=2, a::Integer=3, b::Integer=1) #TODO: | , __cycle_prevention:Optional[Set[int]]=None
 #     # Call out the reverse_function before any magic returns to trap bad values.
 #     _ = reverse_function(initial_value,P=P,a=a,b=b)
 #     tgraph = {initial_value:{}}
@@ -321,5 +327,13 @@ end
 #                 max_orbit_distance-1, P=P, a=a, b=b,
 #                 __cycle_prevention=__cycle_prevention)[branch_value]
 #     return tgraph
+    return 1
+end
 
 end # module
+
+#TODO: < the exports similar to python
+# from .parameterised import *
+# from .parameterised import _ErrMsg
+# from .parameterised import _CC
+# from .parameterised import _KNOWN_CYCLES
