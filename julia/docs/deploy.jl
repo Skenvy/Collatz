37beb6327b29c0b6178b02cbf1702639393fc510
Skenvy <17214791+Skenvy@users.noreply.github.com>
2022-04-07 15:10:16 +1000

    Set the julia docs deploy dirname in a test

--- julia/docs/deploy.jl
+++ julia/docs/deploy.jl
@@ -6,7 +6,8 @@ const _devurl = "dev"
     repo = "github.com/Skenvy/Collatz.git",
     deploy_config = Documenter.GitHubActions(),
     # A special branch for hosting the built docs
-    branch = "gh-pages-julia-docs",
+    branch = "gh-pages-julia-docs-test-dirname",
+    dirname = "julia",
     # Where to deploy to from a non-tagged commit?
     devurl = _devurl,
     devbranch = "main",
