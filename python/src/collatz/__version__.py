4999708ad211dee87dbfe14b8208338924ab3030
Skenvy <17214791+Skenvy@users.noreply.github.com>
2022-04-16 22:24:28 +1000

    Confirm this change stops the CI step from running

--- python/src/collatz/__version__.py
+++ python/src/collatz/__version__.py
@@ -1,2 +1,2 @@
 __version__ = "1.0.0"
-# an edit that will be reverted to check a condition in the workflow
+# an edit that will be reverted to check a condition in the workflow + another edit to see if this stops the test workflow
