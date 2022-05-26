9b1738221fc421d153eabd37c837239b189c6bed
Nathan Levett <17214791+Skenvy@users.noreply.github.com>
2022-05-27 03:03:48 +1000

    Java; Hello World with Workflows for deploying to OSSRH and GitHub (#12)
    * Add sonatype's required properties to the pom
    * Additional changes to the pom and java workflow to add src, javadocs and gpg, and multiple release targets
    * Update the repo url for ossrh to include s01
    * Add snapshot url for github
    * Lowercase the artifact ID
    * Reset the build workflow to only main and set the version to 0.1.0 for hello world + cicd

---
+++ java/src/main/java/io/github/skenvy/Collatz.java
@@ -0,0 +1,13 @@
+package io.github.skenvy;
+
+/**
+ * Hello world!
+ *
+ */
+public class Collatz 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ java/src/test/java/io/github/skenvy/CollatzTest.java
@@ -0,0 +1,20 @@
+package io.github.skenvy;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CollatzTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
