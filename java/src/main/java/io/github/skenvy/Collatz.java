65e002752adf7a0ae1e78a7ddbd6fbcf513fb43c
Skenvy <17214791+Skenvy@users.noreply.github.com>
2022-06-15 01:33:48 +1000

    WIP of the function and reverse function implementation being converted from python

--- java/src/main/java/io/github/skenvy/Collatz.java
+++ java/src/main/java/io/github/skenvy/Collatz.java
@@ -1,13 +1,146 @@
 package io.github.skenvy;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+
 /**
- * Hello world!
- *
+ * Provides the basic functionality to interact with the Collatz conjecture.
+ * The parameterisation uses the same (P,a,b) notation as Conway's generalisations.
+ * Besides the function and reverse function, there is also functionality to
+ * retrieve the hailstone sequence, the "stopping time"/"total stopping time", or
+ * tree-graph.
  */
-public class Collatz 
+public final class Collatz
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    /**
+     * Error message constant.
+     */
+    protected enum _ErrMsg {
+        SANE_PARAMS_P("'P' should not be 0 ~ violates modulo being non-zero."),
+        SANE_PARAMS_A("'a' should not be 0 ~ violates the reversability.");
+
+        private final String label;
+
+        private _ErrMsg(String label) {
+            this.label = label;
+        }
+
+        protected String getLabel(){
+            return this.label;
+        }
+    }
+
+    /**
+     * Cycle Control: Descriptive flags to indicate when some event occurs in the
+     * hailstone sequences, when set to verbose, or stopping time check.
+     */
+    protected enum _CC {
+        STOPPING_TIME("STOPPING_TIME"),
+        TOTAL_STOPPING_TIME("TOTAL_STOPPING_TIME"),
+        CYCLE_INIT("CYCLE_INIT"),
+        CYCLE_LENGTH("CYCLE_LENGTH"),
+        MAX_STOP_OOB("MAX_STOP_OOB"),
+        ZERO_STOP("ZERO_STOP");
+
+        private final String label;
+
+        private _CC(String label) {
+            this.label = label;
+        }
+
+        protected String getLabel(){
+            return this.label;
+        }
+    }
+
+    static protected class FailedSaneParameterCheck extends ArithmeticException { 
+        public FailedSaneParameterCheck(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+
+    /**
+     * Handles the sanity check for the parameterisation (P,a,b) required by both
+     * the function and reverse function.
+     * @param P (BigInteger): Modulus used to devide n, iff n is equivalent to (0 mod P)
+     * @param a (BigInteger): Factor by which to multiply n.
+     * @param b (BigInteger): Value to add to the scaled value of n.
+     */
+    private static void assertSaneParameterisation(BigInteger P, BigInteger a, BigInteger b){
+        /* Sanity check (P,a,b) ~ P absolutely can't be 0. a "could" be zero
+         * theoretically, although would violate the reversability (if ~a is 0 then a
+         * value of "b" as the input to the reverse function would have a pre-emptive
+         * value of every number not divisible by P). The function doesn't _have_ to
+         * be reversable, but we are only interested in dealing with the class of
+         * functions that exhibit behaviour consistant with the collatz function. If
+         * _every_ input not divisable by P went straight to "b", it would simply
+         * cause a cycle consisting of "b" and every b/P^z that is an integer. While
+         * P in [-1, 1] could also be a reasonable check, as it makes every value
+         * either a 1 or 2 length cycle, it's not strictly an illegal operation.
+         * "b" being zero would cause behaviour not consistant with the collatz
+         * function, but would not violate the reversability, so no check either.
+         * " != 0" is redundant for python assertions. */
+        if(P == BigInteger.ZERO){
+            throw new FailedSaneParameterCheck(_ErrMsg.SANE_PARAMS_P.getLabel());
+        } else if(a == BigInteger.ZERO){
+            throw new FailedSaneParameterCheck(_ErrMsg.SANE_PARAMS_A.getLabel());
+        }
+    }
+
+    /**
+     * Returns the output of a single application of a Collatz-esque function.
+     * @param n (BigInteger): The value on which to perform the Collatz-esque function.
+     * @param P (BigInteger): Modulus used to devide n, iff n is equivalent to (0 mod P).
+     * @param a (BigInteger): Factor by which to multiply n.
+     * @param b (BigInteger): Value to add to the scaled value of n.
+     * @return (BigInteger): The result of the function
+     */
+    public static BigInteger function(BigInteger n, BigInteger P, BigInteger a, BigInteger b){
+        assertSaneParameterisation(P,a,b);
+        if(n.remainder(P) == BigInteger.ZERO){
+            return n.divide(P);
+        } else {
+            return n.multiply(a).add(b);
+        }
+    }
+
+    /**
+     * Returns the output of a single application of a Collatz-esque function.
+     * @param n (BigInteger): The value on which to perform the Collatz-esque function.
+     * @return (BigInteger): The result of the function
+     */
+    public static BigInteger function(BigInteger n){
+        return function(n, BigInteger.valueOf(2), BigInteger.valueOf(3), BigInteger.valueOf(1));
+    }
+
+    /**
+     * Returns the output of a single application of a Collatz-esque reverse function.
+     * @param n (BigInteger): The value on which to perform the reverse Collatz function.
+     * @param P (BigInteger): Modulus used to devide n, iff n is equivalent to (0 mod P).
+     * @param a (BigInteger): Factor by which to multiply n.
+     * @param b (BigInteger): Value to add to the scaled value of n.
+     * @return (BigInteger): The result of the function
+     */
+    public static BigInteger[] reverseFunction(BigInteger n, BigInteger P, BigInteger a, BigInteger b){
+        assertSaneParameterisation(P,a,b);
+        /*(n-b)%a == 0 && (n-b)%(P*a) != 0*/
+        if(n.subtract(b).remainder(a) == BigInteger.ZERO && n.subtract(b).remainder(P.multiply(a)) != BigInteger.ZERO){
+            // [P*n] + [(n-b)//a]
+            BigInteger[] preVals = new BigInteger[]{P.multiply(n), n.subtract(b).divide(a)};
+            Arrays.sort(preVals);
+            return preVals;
+        } else { // [P*n]
+            return new BigInteger[]{P.multiply(n)};
+        }
+    }
+
+    /**
+     * Returns the output of a single application of a Collatz-esque reverse function.
+     * @param n (BigInteger): The value on which to perform the reverse Collatz function.
+     * @return (BigInteger): The result of the function
+     */
+    public static BigInteger[] reverseFunction(BigInteger n){
+        return reverseFunction(n, BigInteger.valueOf(2), BigInteger.valueOf(3), BigInteger.valueOf(1));
     }
 }
--- java/src/test/java/io/github/skenvy/CollatzTest.java
+++ java/src/test/java/io/github/skenvy/CollatzTest.java
@@ -1,20 +1,91 @@
 package io.github.skenvy;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
+
+import java.math.BigInteger;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class CollatzTest 
+public class CollatzTest
 {
-    /**
-     * Rigorous Test :-)
-     */
+
+    private static long wrapFunction(long n){
+        return Collatz.function(BigInteger.valueOf(n)).longValue();
+    }
+
+    private static long wrapFunction(long n, long P, long a, long b){
+        return Collatz.function(BigInteger.valueOf(n), BigInteger.valueOf(P), BigInteger.valueOf(a), BigInteger.valueOf(b)).longValue();
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testFunction(){
+        // Default/Any (P,a,b); 0 trap
+        assertEquals(wrapFunction(0), 0);
+        // Default/Any (P,a,b); 1 cycle; positives
+        assertEquals(wrapFunction(1), 4);
+        assertEquals(wrapFunction(4), 2);
+        assertEquals(wrapFunction(2), 1);
+        // Default/Any (P,a,b); -1 cycle; negatives
+        assertEquals(wrapFunction(-1), -2);
+        assertEquals(wrapFunction(-2), -1);
+        // Test a wider modulo sweep by upping P to 5, a to 2, and b to 3.
+        assertEquals(wrapFunction(1, 5, 2, 3), 5);
+        assertEquals(wrapFunction(2, 5, 2, 3), 7);
+        assertEquals(wrapFunction(3, 5, 2, 3), 9);
+        assertEquals(wrapFunction(4, 5, 2, 3), 11);
+        assertEquals(wrapFunction(5, 5, 2, 3), 1);
+        // Test negative P, a and b. Modulo, used in the function, has ambiguous functionality
+        // rather than the more definite euclidean, but we only use it's (0 mod P)
+        // conjugacy class to determine functionality, so the flooring for negative P
+        // doesn't cause any issue.
+        assertEquals(wrapFunction(1, -3, -2, -5), -7);
+        assertEquals(wrapFunction(2, -3, -2, -5), -9);
+        assertEquals(wrapFunction(3, -3, -2, -5), -1);
+        // Set P and a to 0 to assert on assertSaneParameterisation
+        Exception exception;
+        exception = assertThrows(Collatz.FailedSaneParameterCheck.class, () -> {wrapFunction(1, 0, 2, 3);});
+        assertTrue(exception.getMessage().contains(Collatz._ErrMsg.SANE_PARAMS_P.getLabel()));
+        exception = assertThrows(Collatz.FailedSaneParameterCheck.class, () -> {wrapFunction(1, 0, 0, 3);});
+        assertTrue(exception.getMessage().contains(Collatz._ErrMsg.SANE_PARAMS_P.getLabel()));
+        exception = assertThrows(Collatz.FailedSaneParameterCheck.class, () -> {wrapFunction(1, 1, 0, 3);});
+        assertTrue(exception.getMessage().contains(Collatz._ErrMsg.SANE_PARAMS_A.getLabel()));
+    }
+
+    @Test
+    public void testReverseFunction(){
+        // # Default (P,a,b); 0 trap [as b is not a multiple of a]
+        // assert collatz.reverse_function(0) == [0]
+        // # Default (P,a,b); 1 cycle; positives
+        // assert collatz.reverse_function(1) == [2]
+        // assert collatz.reverse_function(4) == [1, 8]
+        // assert collatz.reverse_function(2) == [4]
+        // # Default (P,a,b); -1 cycle; negatives
+        // assert collatz.reverse_function(-1) == [-2]
+        // assert collatz.reverse_function(-2) == [-4, -1]
+        // # Test a wider modulo sweep by upping P to 5, a to 2, and b to 3.
+        // assert collatz.reverse_function(1, P=5, a=2, b=3) == [-1, 5]
+        // assert collatz.reverse_function(2, P=5, a=2, b=3) == [10]
+        // assert collatz.reverse_function(3, P=5, a=2, b=3) == [15]  # also tests !0
+        // assert collatz.reverse_function(4, P=5, a=2, b=3) == [20]
+        // assert collatz.reverse_function(5, P=5, a=2, b=3) == [1, 25]
+        // # Test negative P, a and b. %, used in the function, is "floor" in python
+        // # rather than the more reasonable euclidean, but we only use it's (0 mod P)
+        // # conjugacy class to determine functionality, so the flooring for negative P
+        // # doesn't cause any issue.
+        // assert collatz.reverse_function(1, P=-3, a=-2, b=-5) == [-3]  # != [-3, -3]
+        // assert collatz.reverse_function(2, P=-3, a=-2, b=-5) == [-6]
+        // assert collatz.reverse_function(3, P=-3, a=-2, b=-5) == [-9, -4]
+        // # Set P and a to 0 to assert on __assert_sane_parameterisation
+        // with pytest.raises(AssertionError, match=_REGEX_ERR_P_IS_ZERO):
+        //     collatz.reverse_function(1, P=0, a=2, b=3)
+        // with pytest.raises(AssertionError, match=_REGEX_ERR_P_IS_ZERO):
+        //     collatz.reverse_function(1, P=0, a=0, b=3)
+        // with pytest.raises(AssertionError, match=_REGEX_ERR_A_IS_ZERO):
+        //     collatz.reverse_function(1, P=1, a=0, b=3)
+        // # If b is a multiple of a, but not of Pa, then 0 can have a reverse.
+        // assert collatz.reverse_function(0, P=17, a=2, b=-6) == [0, 3]
+        // assert collatz.reverse_function(0, P=17, a=2, b=102) == [0]
     }
 }
