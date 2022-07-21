bc0b1c0a7cc0856041239a86dec66ae13150c7f1
Skenvy <>
2022-07-22 00:52:13 +1000

    Finish the tree graph tests

--- java/src/test/java/io/github/skenvy/CollatzTest.java
+++ java/src/test/java/io/github/skenvy/CollatzTest.java
@@ -280,74 +280,132 @@ public class CollatzTest
         assertTrue(exception.getMessage().contains(Collatz.SaneParameterErrMsg.SANE_PARAMS_A.getLabel()));
     }
 
-    private static TreeGraphNode wrapTerminalTreeGraphNode(long n){
+    private static TreeGraph wrapTreeGraph(long nodeValue, int maxOrbitDistance, long P, long a, long b){
+        return Collatz.treeGraph(BigInteger.valueOf(nodeValue), maxOrbitDistance, BigInteger.valueOf(P), BigInteger.valueOf(a), BigInteger.valueOf(b));
+    }
+
+    private static TreeGraph wrapTreeGraph(long nodeValue, int maxOrbitDistance){
+        return Collatz.treeGraph(BigInteger.valueOf(nodeValue), maxOrbitDistance);
+    }
+
+    /**
+     * Create a "terminal" graph node with null children and the terminal
+     * condition that indicates it has reached the maximum orbit of the tree.
+     * @param n
+     * @return
+     */
+    private static TreeGraphNode wrapTGN_TerminalNode(long n){
         return new TreeGraphNode(BigInteger.valueOf(n), HailState.MAX_STOP_OUT_OF_BOUNDS, null, null, null);
     }
 
-    private static TreeGraphNode wrapCyclicTerminalTreeGraphNode(long n){
+    /**
+     * Create a "cyclic terminal" graph node with null children and the "cycle termination" condition.
+     * @param n
+     * @return
+     */
+    private static TreeGraphNode wrapTGN_CyclicTerminal(long n){
         return new TreeGraphNode(BigInteger.valueOf(n), HailState.CYCLE_LENGTH, null, null, null);
     }
 
-    private static TreeGraphNode wrapCyclicStartTreeGraphNode(long n, TreeGraphNode preNDivPNode, TreeGraphNode preANplusBNode){
+    /**
+     * Create a "cyclic start" graph node with given children and the "cycle start" condition.
+     * @param n
+     * @param preNDivPNode
+     * @param preANplusBNode
+     * @return
+     */
+    private static TreeGraphNode wrapTGN_CyclicStart(long n, TreeGraphNode preNDivPNode, TreeGraphNode preANplusBNode){
         return new TreeGraphNode(BigInteger.valueOf(n), HailState.CYCLE_INIT, preNDivPNode, preANplusBNode, null);
     }
 
-    private static TreeGraphNode wrapGenericTreeGraphNode(long n, TreeGraphNode preNDivPNode, TreeGraphNode preANplusBNode){
+    /**
+     * Create a graph node with no terminal state, with given children.
+     * @param n
+     * @param preNDivPNode
+     * @param preANplusBNode
+     * @return
+     */
+    private static TreeGraphNode wrapTGN_Generic(long n, TreeGraphNode preNDivPNode, TreeGraphNode preANplusBNode){
         return new TreeGraphNode(BigInteger.valueOf(n), null, preNDivPNode, preANplusBNode, null);
     }
 
     @Test
     public void testTreeGraph(){
         // ":D" for terminal, "C:" for cyclic end
-        TreeGraph expected;
+        TreeGraphNode expectedRoot;
         // The default zero trap
-        /*{0:D}*/ expected = new TreeGraph(wrapTerminalTreeGraphNode(0));
-        assertEquals(expected, Collatz.treeGraph(BigInteger.valueOf(0), 0));
-        /*{0:{C:0}}*/ expected = new TreeGraph(wrapCyclicStartTreeGraphNode(0, wrapCyclicTerminalTreeGraphNode(0), null));
-        assertEquals(expected, Collatz.treeGraph(BigInteger.valueOf(0), 1));
-        assertEquals(expected, Collatz.treeGraph(BigInteger.valueOf(0), 2));
+        // {0:D}
+        expectedRoot = wrapTGN_TerminalNode(0);
+        assertEquals(new TreeGraph(expectedRoot), wrapTreeGraph(0, 0));
+        // {0:{C:0}}
+        expectedRoot = wrapTGN_CyclicStart(0, wrapTGN_CyclicTerminal(0), null);
+        assertEquals(new TreeGraph(expectedRoot), wrapTreeGraph(0, 1));
+        assertEquals(new TreeGraph(expectedRoot), wrapTreeGraph(0, 2));
         // The roundings of the 1 cycle.
-        /*{1:D}*/ expected = new TreeGraph(wrapTerminalTreeGraphNode(1));
-        assertEquals(expected, Collatz.treeGraph(BigInteger.valueOf(1), 0));
-        /*{1:{2:D}}*/ expected = new TreeGraph(wrapGenericTreeGraphNode(1, wrapTerminalTreeGraphNode(2), null));
-        assertEquals(expected, Collatz.treeGraph(BigInteger.valueOf(1), 1));
-        /*{1:{2:{4:D}}}*/ expected = new TreeGraph(wrapGenericTreeGraphNode(1, wrapGenericTreeGraphNode(2, wrapTerminalTreeGraphNode(4), null), null));
-        assertEquals(expected, Collatz.treeGraph(BigInteger.valueOf(1), 2));
-        /*{1:{2:{4:{C:1,8:D}}}}*/ expected = new TreeGraph(wrapCyclicStartTreeGraphNode(1, wrapGenericTreeGraphNode(2, wrapGenericTreeGraphNode(4, wrapTerminalTreeGraphNode(8), wrapCyclicTerminalTreeGraphNode(1)), null), null));
-        assertEquals(expected, Collatz.treeGraph(BigInteger.valueOf(1), 3));
-        /*{2:{4:{1:{C:2},8:{16:D}}}}*/ expected = new TreeGraph(wrapCyclicStartTreeGraphNode(2, wrapGenericTreeGraphNode(4, wrapGenericTreeGraphNode(8, wrapTerminalTreeGraphNode(16), null), wrapGenericTreeGraphNode(1, wrapCyclicTerminalTreeGraphNode(2), null)), null));
-        assertEquals(expected, Collatz.treeGraph(BigInteger.valueOf(2), 3));
-        /*{4:{1:{2:{C:4}},8:{16:{5:D,32:D}}}}*/ expected = new TreeGraph(wrapCyclicStartTreeGraphNode(4, wrapGenericTreeGraphNode(8, wrapGenericTreeGraphNode(16, wrapTerminalTreeGraphNode(32), wrapTerminalTreeGraphNode(5)), null), wrapGenericTreeGraphNode(1, wrapGenericTreeGraphNode(2, wrapCyclicTerminalTreeGraphNode(4), null), null)));
-        assertEquals(expected, Collatz.treeGraph(BigInteger.valueOf(4), 3));
-        // # The roundings of the -1 cycle
-        // assert collatz.tree_graph(-1, 1) == {-1:{-2:D}}
-        // assert collatz.tree_graph(-1, 2) == {-1:{-2:{-4:D,C:-1}}}
-        // # Test a wider modulo sweep by upping P to 5, a to 2, and b to 3.
-        // T = lambda x,y: {1:{-1:x,5:y}}
-        // orb_1 = T(D,D)
-        // orb_2 = T({-5:D,-2:D},{C:1,25:D})
-        // T = lambda x,y,z: {1:{-1:{-5:x,-2:y},5:{C:1,25:z}}}
-        // orb_3 = T({-25:D,-4:D},{-10:D},{11:D,125:D})
-        // assert collatz.tree_graph(1, 1, P=5, a=2, b=3) == orb_1
-        // assert collatz.tree_graph(1, 2, P=5, a=2, b=3) == orb_2
-        // assert collatz.tree_graph(1, 3, P=5, a=2, b=3) == orb_3
-        // # Test negative P, a and b.
-        // orb_1 = {1:{-3:D}}
-        // T = lambda x,y: {1:{-3:{-1:x,9:y}}}
-        // orb_2 = T(D,D)
-        // orb_3 = T({-2:D,3:D},{-27:D,-7:D})
-        // assert collatz.tree_graph(1, 1, P=-3, a=-2, b=-5) == orb_1
-        // assert collatz.tree_graph(1, 2, P=-3, a=-2, b=-5) == orb_2
-        // assert collatz.tree_graph(1, 3, P=-3, a=-2, b=-5) == orb_3
-        // # Set P and a to 0 to assert on __assert_sane_parameterisation
-        // with pytest.raises(AssertionError, match=_REGEX_ERR_P_IS_ZERO):
-        //     collatz.tree_graph(1, 1, P=0, a=2, b=3)
-        // with pytest.raises(AssertionError, match=_REGEX_ERR_P_IS_ZERO):
-        //     collatz.tree_graph(1, 1, P=0, a=0, b=3)
-        // with pytest.raises(AssertionError, match=_REGEX_ERR_A_IS_ZERO):
-        //     collatz.tree_graph(1, 1, P=1, a=0, b=3)
-        // # If b is a multiple of a, but not of Pa, then 0 can have a reverse.
-        // assert collatz.tree_graph(0, 1, P=17, a=2, b=-6) == {0:{C:0,3:D}}
-        // assert collatz.tree_graph(0, 1, P=17, a=2, b=102) == {0:{C:0}}
+        // {1:D}
+        expectedRoot = wrapTGN_TerminalNode(1);
+        assertEquals(new TreeGraph(expectedRoot), wrapTreeGraph(1, 0));
+        // {1:{2:D}}
+        expectedRoot = wrapTGN_Generic(1, wrapTGN_TerminalNode(2), null);
+        assertEquals(new TreeGraph(expectedRoot), wrapTreeGraph(1, 1));
+        // {1:{2:{4:D}}}
+        expectedRoot = wrapTGN_Generic(1, wrapTGN_Generic(2, wrapTGN_TerminalNode(4), null), null);
+        assertEquals(new TreeGraph(expectedRoot), wrapTreeGraph(1, 2));
+        // {1:{2:{4:{C:1,8:D}}}}
+        expectedRoot = wrapTGN_CyclicStart(1, wrapTGN_Generic(2, wrapTGN_Generic(4, wrapTGN_TerminalNode(8), wrapTGN_CyclicTerminal(1)), null), null);
+        assertEquals(new TreeGraph(expectedRoot), wrapTreeGraph(1, 3));
+        // {2:{4:{1:{C:2},8:{16:D}}}}
+        expectedRoot = wrapTGN_CyclicStart(2, wrapTGN_Generic(4, wrapTGN_Generic(8, wrapTGN_TerminalNode(16), null),
+                                                                 wrapTGN_Generic(1, wrapTGN_CyclicTerminal(2), null)), null);
+        assertEquals(new TreeGraph(expectedRoot), wrapTreeGraph(2, 3));
+        // {4:{1:{2:{C:4}},8:{16:{5:D,32:D}}}}
+        expectedRoot = wrapTGN_CyclicStart(4, wrapTGN_Generic(8, wrapTGN_Generic(16, wrapTGN_TerminalNode(32), wrapTGN_TerminalNode(5)), null),
+                                              wrapTGN_Generic(1, wrapTGN_Generic(2, wrapTGN_CyclicTerminal(4), null), null));
+        assertEquals(new TreeGraph(expectedRoot), wrapTreeGraph(4, 3));
+        // The roundings of the -1 cycle
+        // {-1:{-2:D}}
+        expectedRoot = wrapTGN_Generic(-1, wrapTGN_TerminalNode(-2), null);
+        assertEquals(new TreeGraph(expectedRoot), wrapTreeGraph(-1, 1));
+        // {-1:{-2:{-4:D,C:-1}}}
+        expectedRoot = wrapTGN_CyclicStart(-1, wrapTGN_Generic(-2, wrapTGN_TerminalNode(-4), wrapTGN_CyclicTerminal(-1)), null);
+        assertEquals(new TreeGraph(expectedRoot), wrapTreeGraph(-1, 2));
+        // Test a wider modulo sweep by upping P to 5, a to 2, and b to 3.
+        // Orbit distance of 1 ~= {1:{-1:D,5:D}}
+        expectedRoot = wrapTGN_Generic(1, wrapTGN_TerminalNode(5), wrapTGN_TerminalNode(-1));
+        assertEquals(new TreeGraph(expectedRoot), wrapTreeGraph(1, 1, 5, 2, 3));
+        // Orbit distance of 2 ~= {1:{-1:{-5:D,-2:D},5:{C:1,25:D}}}
+        expectedRoot = wrapTGN_CyclicStart(1, wrapTGN_Generic(5, wrapTGN_TerminalNode(25), wrapTGN_CyclicTerminal(1)),
+                                              wrapTGN_Generic(-1, wrapTGN_TerminalNode(-5), wrapTGN_TerminalNode(-2)));
+        assertEquals(new TreeGraph(expectedRoot), wrapTreeGraph(1, 2, 5, 2, 3));
+        // Orbit distance of 3 ~=  {1:{-1:{-5:{-25:D,-4:D},-2:{-10:D}},5:{C:1,25:{11:D,125:D}}}}
+        expectedRoot = wrapTGN_CyclicStart(1, wrapTGN_Generic(5, wrapTGN_Generic(25, wrapTGN_TerminalNode(125), wrapTGN_TerminalNode(11)), wrapTGN_CyclicTerminal(1)),
+                                              wrapTGN_Generic(-1, wrapTGN_Generic(-5, wrapTGN_TerminalNode(-25), wrapTGN_TerminalNode(-4)), wrapTGN_Generic(-2, wrapTGN_TerminalNode(-10), null)));
+        assertEquals(new TreeGraph(expectedRoot), wrapTreeGraph(1, 3, 5, 2, 3));
+        // Test negative P, a and b ~ P=-3, a=-2, b=-5
+        // Orbit distance of 1 ~= {1:{-3:D}}
+        expectedRoot = wrapTGN_Generic(1, wrapTGN_TerminalNode(-3), null);
+        assertEquals(new TreeGraph(expectedRoot), wrapTreeGraph(1, 1, -3, -2, -5));
+        // Orbit distance of 2 ~= {1:{-3:{-1:D,9:D}}}
+        expectedRoot = wrapTGN_Generic(1, wrapTGN_Generic(-3, wrapTGN_TerminalNode(9), wrapTGN_TerminalNode(-1)), null);
+        assertEquals(new TreeGraph(expectedRoot), wrapTreeGraph(1, 2, -3, -2, -5));
+        // Orbit distance of 3 ~= {1:{-3:{-1:{-2:D,3:D},9:{-27:D,-7:D}}}}
+        expectedRoot = wrapTGN_Generic(1, wrapTGN_Generic(-3, wrapTGN_Generic(9, wrapTGN_TerminalNode(-27), wrapTGN_TerminalNode(-7)),
+                                                              wrapTGN_Generic(-1, wrapTGN_TerminalNode(3), wrapTGN_TerminalNode(-2))), null);
+        assertEquals(new TreeGraph(expectedRoot), wrapTreeGraph(1, 3, -3, -2, -5));
+        // Set P and a to 0 to assert on __assert_sane_parameterisation
+        Exception exception;
+        exception = assertThrows(Collatz.FailedSaneParameterCheck.class, () -> {wrapTreeGraph(1, 1, 0, 2, 3);});
+        assertTrue(exception.getMessage().contains(Collatz.SaneParameterErrMsg.SANE_PARAMS_P.getLabel()));
+        exception = assertThrows(Collatz.FailedSaneParameterCheck.class, () -> {wrapTreeGraph(1, 1, 0, 0, 3);});
+        assertTrue(exception.getMessage().contains(Collatz.SaneParameterErrMsg.SANE_PARAMS_P.getLabel()));
+        exception = assertThrows(Collatz.FailedSaneParameterCheck.class, () -> {wrapTreeGraph(1, 1, 1, 0, 3);});
+        assertTrue(exception.getMessage().contains(Collatz.SaneParameterErrMsg.SANE_PARAMS_A.getLabel()));
+        // If b is a multiple of a, but not of Pa, then 0 can have a reverse.
+        // {0:{C:0,3:D}}
+        expectedRoot = wrapTGN_CyclicStart(0, wrapTGN_CyclicTerminal(0), wrapTGN_TerminalNode(3));
+        assertEquals(new TreeGraph(expectedRoot), wrapTreeGraph(0, 1, 17, 2, -6));
+        // {0:{C:0}}
+        expectedRoot = wrapTGN_CyclicStart(0, wrapTGN_CyclicTerminal(0), null);
+        assertEquals(new TreeGraph(expectedRoot), wrapTreeGraph(0, 1, 17, 2, 102));
     }
 }
