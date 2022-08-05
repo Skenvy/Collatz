a4d9a5827777595eb03fa712441a8fb53f3a0424
Nathan Levett <17214791+Skenvy@users.noreply.github.com>
2022-08-06 02:57:41 +1000

    Go v0.4.0: StoppingTime (#46)
    * Add coverage to the test output
    * Tests for stopping time work
    * Go v0.4.0: StoppingTime
    Co-authored-by: Skenvy <>

--- go/collatz.go
+++ go/collatz.go
@@ -377,3 +377,60 @@ func HailstoneSequenceDefault(initialValue *big.Int, maxTotalStoppingTime int) *
 	ret, _ := ParameterisedHailstoneSequence(initialValue, DEFAULT_P(), DEFAULT_A(), DEFAULT_B(), maxTotalStoppingTime, true)
 	return ret
 }
+
+// Returns the stopping time, the amount of iterations required to reach a
+// value less than the initial value, or -Inf if maxStoppingTime is exceeded.
+// Alternatively, if totalStoppingTime is True, then it will instead count
+// the amount of iterations to reach 1. If the sequence does not stop, but
+// instead ends in a cycle, the result will be +Inf.
+// If (P,a,b) are such that it is possible to get stuck on zero, the result
+// will be the negative of what would otherwise be the "total stopping time"
+// to reach 1, where 0 is considered a "total stop" that should not occur as
+// it does form a cycle of length 1.
+//     initialValue (*big.Int): The value for which to find the stopping time.
+//     P (*big.Int): Modulus used to devide n, iff n is equivalent to (0 mod P).
+//     a (*big.Int): Factor by which to multiply n.
+//     b (*big.Int): Value to add to the scaled value of n.
+//     maxStoppingTime (int): Maximum amount of times to iterate the function, if
+//          the stopping time is not reached. IF the maxStoppingTime is reached,
+//          the function will return null.
+//     totalStoppingTime (bool): Whether or not to execute until the "total" stopping
+//          time (number of iterations to obtain 1) rather than the regular stopping
+//          time (number of iterations to reach a value less than the initial value).
+// return (float64): The stopping time, or, in a special case, infinity, null or a negative.
+func ParameterisedStoppingTime(initialValue *big.Int, P *big.Int, a *big.Int, b *big.Int, maxStoppingTime int, totalStoppingTime bool) (float64, error) {
+	// The information is contained in the hailstone sequence. Although the "max~time"
+	// for hailstones is named for "total stopping" time and the "max~time" for this
+	// "stopping time" function is _not_ "total", they are handled the same way, as
+	// the default for "totalStoppingTime" for hailstones is true, but for this, is
+	// false. Thus the naming difference.
+	hail, err := ParameterisedHailstoneSequence(initialValue, P, a, b, maxStoppingTime, totalStoppingTime)
+	// For total/regular/zero stopping time, the value is already the same as
+	// that present, for cycles we report infinity instead of the cycle length,
+	// and for max stop out of bounds, we report null instead of the max stop cap
+	if err != nil {
+		return math.Inf(-1), err
+	}
+	switch hail.terminalCondition {
+	case STOPPING_TIME:
+		return float64(hail.terminalStatus), nil
+	case TOTAL_STOPPING_TIME:
+		return float64(hail.terminalStatus), nil
+	case CYCLE_LENGTH:
+		return math.Inf(1), nil
+	case MAX_STOP_OUT_OF_BOUNDS:
+		return math.Inf(-1), nil
+	case ZERO_STOP:
+		return float64(hail.terminalStatus), nil
+	}
+	return math.Inf(-1), nil
+}
+
+// Returns the stopping time, the amount of iterations required to reach a
+// value less than the initial value, or -Inf if maxStoppingTime is exceeded.
+//     initialValue (*big.Int): The value for which to find the stopping time.
+// return (float64): The stopping time, or, in a cycle case, infinity.
+func StoppingTime(initialValue *big.Int) float64 {
+	stop, _ := ParameterisedStoppingTime(initialValue, DEFAULT_P(), DEFAULT_A(), DEFAULT_B(), 1000, false)
+	return stop
+}
