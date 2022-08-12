d546627c272bbf00b3a23fe05e2f704402d71bb6
Skenvy <17214791+Skenvy@users.noreply.github.com>
2022-08-12 19:20:20 +1000

    Add a recipe setup_debian for all the dependencies I was missing for devtools.

--- R/R/Collatz.R
+++ R/R/Collatz.R
@@ -1,1 +1,27 @@
-print("Hello World!", quote = FALSE)
+#' The four known cycles for the standard parameterisation.
+#' As a "list of lists", you can iterate over them as
+#' for (KC in KNOWN.CYCLES[[1]]) with an inner for (val in KC)
+KNOWN.CYCLES <- list(c(list(c(1, 4, 2)), list(c(-1, -2)), list(c(-5, -14, -7, -20, -10)),
+    list(c(-17, -50, -25, -74, -37, -110, -55, -164, -82, -41, -122, -61, -182, -91, -272, -136, -68, -34))))
+lockBinding("KNOWN.CYCLES", globalenv())
+
+# for (KC in KNOWN.CYCLES[[1]]){
+#     print(KC)
+#     for (val in KC){
+#         print(val)
+#     }
+# }
+
+# too large to add until implementing arb ints
+# VERIFIED.MAXIMUM <- 295147905179352825856
+# lockBinding("VERIFIED.MAXIMUM", globalenv())
+
+VERIFIED.MINIMUM <- -272
+lockBinding("VERIFIED.MINIMUM", globalenv())
+
+SaneParameterErrMsg <- list(c(P="'P' should not be 0 ~ violates modulo being non-zero.", A="'a' should not be 0 ~ violates the reversability."))
+lockBinding("SaneParameterErrMsg", globalenv())
+
+SequenceState <- list()
+
+print(VERIFIED.MINIMUM)
