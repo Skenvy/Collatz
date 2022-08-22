ee82dfdddb483483b9029ff8d0c1aa2e4db4e1c0
Skenvy <17214791+Skenvy@users.noreply.github.com>
2022-08-23 01:24:04 +1000

    Move around functionality in R and redo man

---
+++ R/R/collatzFunction.R
@@ -0,0 +1,26 @@
+#' @include utils.R
+NULL
+
+#' The Collatz function
+#'
+#' Returns the output of a single application of a Collatz-esque function.
+#'
+#' This function will compute and return the result of applying one iteration
+#' of a parameterised Collatz-esque function. Although it will operate with
+#' integer inputs, for overflow safety, provide a gmp Big Integer ('bigz').
+#'
+#' @param n (numeric|bigz|bigq) The value on which
+#' to perform the Collatz-esque function
+#' @param P (numeric|bigz|bigq): Modulus used to divide
+#' n, iff n is equivalent to (0 mod P). Default is 2.
+#' @param a (numeric|bigz|bigq) Factor by which to multiply n. Default is 3.
+#' @param b (numeric|bigz|bigq) Value to add
+#' to the scaled value of n. Default is 1.
+#' @returns a numeric, either in-built or a bigz | bigq from the gmp library.
+#' If either n or P are bigz, then the result of n/P will be a bigq
+#' although it's denominator(~) will return 1.
+#' @export
+collatzFunction <- function(n, P=2, a=3, b=1){
+    assertSaneParameterication(P,a,b)
+    if (n%%P == 0) (n/P) else ((a*n)+b)
+}
---
+++ R/R/reverseFunction.R
@@ -0,0 +1,36 @@
+#' @include utils.R
+NULL
+
+#' The "inverse"/"reverse" Collatz function
+#'
+#' Calculates the values that would return the input under the Collatz function.
+#'
+#' Returns the output of a single application of a Collatz-esque reverse
+#' function. If only one value is returned, it is the value that would be
+#' divided by P. If two values are returned, the first is the value that
+#' would be divided by P, and the second value is that which would undergo
+#' the multiply and add step, regardless of which is larger.
+#' @param n (numeric|bigz|bigq) The value on which
+#' to perform the reverse Collatz function
+#' @param P (numeric|bigz|bigq) Modulus used to divide
+#' n, iff n is equivalent to (0 mod P) Default is 2.
+#' @param a (numeric|bigz|bigq) Factor by which to multiply n. Default is 3.
+#' @param b (numeric|bigz|bigq) Value to add
+#' to the scaled value of n. Default is 1.
+#' @returns A vector of either numeric or bigz | bigq type
+#' @export
+reverseFunction <- function(n, P=2, a=3, b=1){
+    assertSaneParameterication(P,a,b)
+    # Every input can be reversed as the result of "n/P" division, which yields
+    # "Pn"... {f(n) = an + b}~={(f(n) - b)/a = n} ~ if n was such that the
+    # muliplication step was taken instead of the division by the modulus, then
+    # (f(n) - b)/a) must be an integer that is not in (0 mod P). Because we're
+    # not placing restrictions on the parameters yet, although there is a better
+    # way of shortcutting this for the default variables, we need to always
+    # attempt (f(n) - b)/a)
+    pre.values <- c(P*n)
+    if ((n-b)%%a == 0 && (n-b)%%(P*a) != 0){
+        pre.values <- append(pre.values, (n-b)/a)
+    }
+    pre.values
+}
