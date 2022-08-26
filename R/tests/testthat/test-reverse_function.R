f6502bd72b6e0e9d263ec52fae5b522a6ad769a3
Skenvy <17214791+Skenvy@users.noreply.github.com>
2022-08-27 01:20:01 +1000

    Change c(~) to list(~) in reverse function test to match the change in the function

--- R/tests/testthat/test-reverse_function.R
+++ R/tests/testthat/test-reverse_function.R
@@ -1,28 +1,28 @@
 test_that("reverse_function_ZeroTrap", {
     # Default (P,a,b) 0 trap [as b is not a multiple of a]
-    expect_equal(reverse_function(0), c(0))
+    expect_equal(reverse_function(0), list(0))
 })
 
 test_that("reverse_function_OneCycle", {
     # Default (P,a,b) 1 cycle positives
-    expect_equal(reverse_function(1), c(2))
-    expect_equal(reverse_function(4), c(8, 1))
-    expect_equal(reverse_function(2), c(4))
+    expect_equal(reverse_function(1), list(2))
+    expect_equal(reverse_function(4), list(8, 1))
+    expect_equal(reverse_function(2), list(4))
 })
 
 test_that("reverse_function_NegativeOneCycle", {
     # Default (P,a,b) -1 cycle negatives
-    expect_equal(reverse_function(-1), c(-2))
-    expect_equal(reverse_function(-2), c(-4, -1))
+    expect_equal(reverse_function(-1), list(-2))
+    expect_equal(reverse_function(-2), list(-4, -1))
 })
 
 test_that("reverse_function_WiderModuloSweep", {
     # Test a wider modulo sweep by upping P to 5, a to 2, and b to 3.
-    expect_equal(reverse_function(1, 5, 2, 3), c(5, -1))
-    expect_equal(reverse_function(2, 5, 2, 3), c(10))
-    expect_equal(reverse_function(3, 5, 2, 3), c(15)) # also tests !0
-    expect_equal(reverse_function(4, 5, 2, 3), c(20))
-    expect_equal(reverse_function(5, 5, 2, 3), c(25, 1))
+    expect_equal(reverse_function(1, 5, 2, 3), list(5, -1))
+    expect_equal(reverse_function(2, 5, 2, 3), list(10))
+    expect_equal(reverse_function(3, 5, 2, 3), list(15)) # also tests !0
+    expect_equal(reverse_function(4, 5, 2, 3), list(20))
+    expect_equal(reverse_function(5, 5, 2, 3), list(25, 1))
 })
 
 test_that("reverse_function_NegativeParamterisation", {
@@ -30,15 +30,15 @@ test_that("reverse_function_NegativeParamterisation", {
     # rather than the more reasonable euclidean, but we only use it's (0 mod P)
     # conjugacy class to determine functionality, so the flooring for negative P
     # doesn't cause any issue.
-    expect_equal(reverse_function(1, -3, -2, -5), c(-3)) # != [-3, -3]
-    expect_equal(reverse_function(2, -3, -2, -5), c(-6))
-    expect_equal(reverse_function(3, -3, -2, -5), c(-9, -4))
+    expect_equal(reverse_function(1, -3, -2, -5), list(-3)) # != [-3, -3]
+    expect_equal(reverse_function(2, -3, -2, -5), list(-6))
+    expect_equal(reverse_function(3, -3, -2, -5), list(-9, -4))
 })
 
 test_that("reverse_function_ZeroReversesOnB", {
     # If b is a multiple of a, but not of Pa, then 0 can have a reverse.
-    expect_equal(reverse_function(0, 17, 2, -6), c(0, 3))
-    expect_equal(reverse_function(0, 17, 2, 102), c(0))
+    expect_equal(reverse_function(0, 17, 2, -6), list(0, 3))
+    expect_equal(reverse_function(0, 17, 2, 102), list(0))
 })
 
 test_that("reverse_function_AssertSaneParameterisation", {
