542a158b793437505ae4f326e9b395b34577227d
Skenvy <17214791+Skenvy@users.noreply.github.com>
2022-09-23 02:49:21 +1000

    Split constants into new subfolder rb file

--- ruby/lib/collatz.rb
+++ ruby/lib/collatz.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "collatz/version"
+require_relative "collatz/constants"
 
 # Provides the basic functionality to interact with the Collatz conjecture. The
 # parameterisation uses the same (p,a,b) notation as Conway's generalisations.
@@ -13,24 +14,6 @@ module Collatz
   # https://github.com/rubocop/ruby-style-guide#modules-vs-classes
   module_function # rubocop:disable Layout/EmptyLinesAroundAccessModifier, Style/AccessModifierDeclarations
 
-  # The four known cycles for the standard parameterisation.
-  KNOWN_CYCLES = [
-    [1, 4, 2], [-1, -2], [-5, -14, -7, -20, -10],
-    [-17, -50, -25, -74, -37, -110, -55, -164, -82, -41, -122, -61, -182, -91, -272, -136, -68, -34]
-  ].each(&:freeze).freeze
-  # The current value up to which the standard parameterisation has been verified.
-  VERIFIED_MAXIMUM = 295147905179352825856
-  # The current value down to which the standard parameterisation has been verified.
-  VERIFIED_MINIMUM = -272 # TODO: Check the actual lowest bound.
-
-  # Error message constants, to be used as input to the FailedSaneParameterCheck.
-  module SaneParameterErrMsg
-    # Message to print in the FailedSaneParameterCheck if p, the modulus, is zero.
-    SANE_PARAMS_P = "'p' should not be 0 ~ violates modulo being non-zero."
-    # Message to print in the FailedSaneParameterCheck if a, the multiplicand, is zero.
-    SANE_PARAMS_A = "'a' should not be 0 ~ violates the reversability."
-  end
-
   # Thrown when either p, the modulus, or a, the multiplicand, are zero.
   class FailedSaneParameterCheck < StandardError
     # Construct a FailedSaneParameterCheck with a message associated with the provided enum.
@@ -39,29 +22,6 @@ module Collatz
       @exception_type = exception_type
       super(msg)
     end
-  end
-
-  # SequenceState for Cycle Control: Descriptive flags to indicate when some event occurs in the
-  # hailstone sequences or tree graph reversal, when set to verbose, or stopping time check.
-  module SequenceState
-    # A Hailstone sequence state that indicates the stopping
-    # time, a value less than the initial, has been reached.
-    STOPPING_TIME = "STOPPING_TIME"
-    # A Hailstone sequence state that indicates the total
-    # stopping time, a value of 1, has been reached.
-    TOTAL_STOPPING_TIME = "TOTAL_STOPPING_TIME"
-    # A Hailstone and TreeGraph sequence state that indicates the
-    # first occurence of a value that subsequently forms a cycle.
-    CYCLE_INIT = "CYCLE_INIT"
-    # A Hailstone and TreeGraph sequence state that indicates the
-    # last occurence of a value that has already formed a cycle.
-    CYCLE_LENGTH = "CYCLE_LENGTH"
-    # A Hailstone and TreeGraph sequence state that indicates the sequence
-    # or traversal has executed some imposed 'maximum' amount of times.
-    MAX_STOP_OUT_OF_BOUNDS = "MAX_STOP_OUT_OF_BOUNDS"
-    # A Hailstone sequence state that indicates the sequence terminated
-    # by reaching "0", a special type of "stopping time".
-    ZERO_STOP = "ZERO_STOP"
   end
 
   # Handles the sanity check for the parameterisation (p,a,b)
