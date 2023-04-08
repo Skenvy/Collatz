9f521d53c8925dea9357c4c017ef18b2ecf1115e
Skenvy <>
2023-04-09 00:02:17 +1000

    WIP fuse together snippets from the julia, java, and python implementations to start off what is probably a terribly formatted TS namespace

--- javascript/lib/esm/types/index.d.ts
+++ javascript/lib/esm/types/index.d.ts
@@ -1,8 +1,33 @@
-export declare function helloWorld(): string;
-export declare function goodBye(): string;
+export declare namespace Collatz {
+    const _KNOWN_CYCLES: bigint[][];
+    const __VERIFIED_MAXIMUM: bigint;
+    const __VERIFIED_MINIMUM: bigint;
+    enum _ErrMsg {
+        SANE_PARAMS_P = "'P' should not be 0 ~ violates modulo being non-zero.",
+        SANE_PARAMS_A = "'a' should not be 0 ~ violates the reversability."
+    }
+    class FailedSaneParameterCheck extends Error {
+        constructor(message: _ErrMsg);
+    }
+    enum _CC {
+        STOPPING_TIME = "STOPPING_TIME",
+        TOTAL_STOPPING_TIME = "TOTAL_STOPPING_TIME",
+        CYCLE_INIT = "CYCLE_INIT",
+        CYCLE_LENGTH = "CYCLE_LENGTH",
+        MAX_STOP_OUT_OF_BOUNDS = "MAX_STOP_OUT_OF_BOUNDS",
+        ZERO_STOP = "ZERO_STOP"
+    }
+    function __assert_sane_parameterisation(P: bigint, a: bigint, b: bigint): void;
+    interface Parameterised {
+        n: bigint;
+        P?: bigint;
+        a?: bigint;
+        b?: bigint;
+    }
+    function Function({ n, P, a, b }: Parameterised): bigint;
+}
 declare const _default: {
-    helloWorld: typeof helloWorld;
-    goodBye: typeof goodBye;
+    Collatz: typeof Collatz;
 };
 export default _default;
 //# sourceMappingURL=index.d.ts.map
--- javascript/tests/index.spec.ts
+++ javascript/tests/index.spec.ts
@@ -1,43 +1,71 @@
-// This is an example test suite from
-// https://github.com/snyk-snippets/modern-npm-package/blob/main/tests/index.spec.ts
-// MIT: https://github.com/snyk-snippets/modern-npm-package/blob/main/LICENSE
-
 import 'mocha';
 import { assert } from 'chai';
 
-import { helloWorld, goodBye } from '../src/index';
-import npmPackage from '../src/index';
+import { Collatz } from '../src/index';
+import defaultExport from '../src/index';
 
 describe('NPM Package', () => {
   it('should be an object', () => {
-    assert.isObject(npmPackage);
+    assert.isObject(defaultExport);
   });
 
-  it('should have a helloWorld property', () => {
-    assert.property(npmPackage, 'helloWorld');
-  });
-});
-
-describe('Hello World Function', () => {
-  it('should be a function', () => {
-    assert.isFunction(helloWorld);
-  });
-
-  it('should return the hello world message', () => {
-    const expected = 'Hello World from my example modern npm package!';
-    const actual = helloWorld();
-    assert.equal(actual, expected);
+  it('should have a Collatz property', () => {
+    assert.property(defaultExport, 'Collatz');
   });
 });
 
-describe('Goodbye Function', () => {
+describe('Collatz', () => {
+  it('should be a namespace', () => {
+    assert.isObject(Collatz);
+  });
+});
+
+describe('Collatz.Function', () => {
   it('should be a function', () => {
-    assert.isFunction(goodBye);
+    assert.isFunction(Collatz.Function);
   });
 
-  it('should return the goodbye message', () => {
-    const expected = 'Goodbye from my example modern npm package!';
-    const actual = goodBye();
+  // Default/Any (P,a,b); 0 trap
+  it('should return the Default/Any (P,a,b); 0 trap', () => {
+    const expected = 0n;
+    const actual = Collatz.Function({n:0n});
     assert.equal(actual, expected);
+  });
+
+  // Default/Any (P,a,b); 1 cycle; positives
+  it('should return the Default/Any (P,a,b); 1 cycle; positives', () => {
+    assert.equal(Collatz.Function({n:1n}), 4n);
+    assert.equal(Collatz.Function({n:4n}), 2n);
+    assert.equal(Collatz.Function({n:2n}), 1n);
+  });
+
+  // Default/Any (P,a,b); -1 cycle; negatives
+  it('should return the Default/Any (P,a,b); -1 cycle; negatives', () => {
+    assert.equal(Collatz.Function({n:-1n}), -2n);
+    assert.equal(Collatz.Function({n:-2n}), -1n);
+  });
+
+  // Test a wider modulo sweep by upping P to 5, a to 2, and b to 3.
+  it('should return the Test a wider modulo sweep by upping P to 5, a to 2, and b to 3.', () => {
+    assert.equal(Collatz.Function({n:1n, P:5n, a:2n, b:3n}), 5n);
+    assert.equal(Collatz.Function({n:2n, P:5n, a:2n, b:3n}), 7n);
+    assert.equal(Collatz.Function({n:3n, P:5n, a:2n, b:3n}), 9n);
+    assert.equal(Collatz.Function({n:4n, P:5n, a:2n, b:3n}), 11n);
+    assert.equal(Collatz.Function({n:5n, P:5n, a:2n, b:3n}), 1n);
+  });
+
+  // Test negative P, a and b. We only use the (0 mod P) conjugacy class to
+  // determine functionality, so flooring negative P doesn't cause any issue.
+  it('should return the Test negative P, a and b.', () => {
+    assert.equal(Collatz.Function({n:1n, P:-3n, a:-2n, b:-5n}), -7n);
+    assert.equal(Collatz.Function({n:2n, P:-3n, a:-2n, b:-5n}), -9n);
+    assert.equal(Collatz.Function({n:3n, P:-3n, a:-2n, b:-5n}), -1n);
+  });
+
+  // Set P and a to 0 to assert on __assert_sane_parameterisation.
+  it('should return the assertion error', () => {
+    assert.throws(() => { Collatz.Function({n:1n, P:0n, a:2n, b:3n}) }, Collatz.FailedSaneParameterCheck, Collatz._ErrMsg.SANE_PARAMS_P);
+    assert.throws(() => { Collatz.Function({n:1n, P:0n, a:0n, b:3n}) }, Collatz.FailedSaneParameterCheck, Collatz._ErrMsg.SANE_PARAMS_P);
+    assert.throws(() => { Collatz.Function({n:1n, P:1n, a:0n, b:3n}) }, Collatz.FailedSaneParameterCheck, Collatz._ErrMsg.SANE_PARAMS_A);
   });
 });
