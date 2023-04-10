8dc285113824d0ad992ca28391221173f452d716
Nathan Levett <17214791+Skenvy@users.noreply.github.com>
2023-04-11 09:53:44 +1000

    Javascript 0.2.0 -- Function and Reverse Function; linting and doccing. (#148)
    * WIP fuse together snippets from the julia, java, and python implementations to start off what is probably a terribly formatted TS namespace
    * Print the first layer of deps installed in ci
    * Change the minimum node version to 14.
    * Update engine requires and install eslint
    * Add a linting step to CI (this push will fail on linting)
    * Try adding AirBnB eslint config
    * Format devlog
    * Add the reverse function
    * Tested and linted the reverse function
    * Add a recipe to fail if the checked in JS mismatches with the checked in TS
    * Install typedoc and tsdoc lint
    * Add tsdoc linter and try to make comments tsdoc-ish
    * Recipe for docs
    * Comment on attempt at JSDoc
    * Comment about setting up the docs target branch
    * Bump up the version to test the whole CICD for javascript, with the function and reverse function
    * Retranspile the js files
    * Add the devDependencies to the docs step
    * Stop windows from corrupting the line endings that eslint requires
    ---------
    Co-authored-by: Skenvy <>

--- javascript/lib/esm/index.mjs
+++ javascript/lib/esm/index.mjs
@@ -1,15 +1,137 @@
-// This is an example src from
-// https://github.com/snyk-snippets/modern-npm-package/blob/main/src/index.ts
-// MIT: https://github.com/snyk-snippets/modern-npm-package/blob/main/LICENSE
-export function helloWorld() {
-    const message = 'Hello World from my example modern npm package!';
-    return message;
+/** The four known cycles (besides 0 cycling to itself), for the default parameterisation. */
+export const KNOWN_CYCLES = [[1n, 4n, 2n], [-1n, -2n], [-5n, -14n, -7n, -20n, -10n],
+    [-17n, -50n, -25n, -74n, -37n, -110n, -55n, -164n, -82n, -41n, -122n, -61n, -182n, -91n, -272n, -136n, -68n, -34n]];
+/** The current value up to which has been proven numerically, for the default parameterisation. */
+export const VERIFIED_MAXIMUM = 295147905179352825856n;
+/** The current value down to which has been proven numerically, for the default parameterisation. */
+export const VERIFIED_MINIMUM = -272n; // TODO: Check the actual lowest bound.
+/**
+ * Error message constants.
+ * @remarks
+ * To be used as input to the FailedSaneParameterCheck.
+ */
+export var SaneParameterErrMsg;
+(function (SaneParameterErrMsg) {
+    /** Message to print in the FailedSaneParameterCheck if P, the modulus, is zero. */
+    SaneParameterErrMsg["SANE_PARAMS_P"] = "'P' should not be 0 ~ violates modulo being non-zero.";
+    /** Message to print in the FailedSaneParameterCheck if a, the multiplicand, is zero. */
+    SaneParameterErrMsg["SANE_PARAMS_A"] = "'a' should not be 0 ~ violates the reversability.";
+})(SaneParameterErrMsg || (SaneParameterErrMsg = {}));
+/**
+ * FailedSaneParameterCheck
+ * @remarks
+ * An Error thrown when assertSaneParameterisation determines invalid parameterisation.
+ * This is when either P, the modulus, or a, the multiplicand, are zero.
+ */
+export class FailedSaneParameterCheck extends Error {
+    /**
+     * Construct a FailedSaneParameterCheck with a message associated with the provided enum.
+     * @param message - The enum from which to extract the message.
+     */
+    constructor(message) {
+        super(message);
+        this.name = 'FailedSaneParameterCheck';
+    }
 }
-export function goodBye() {
-    const message = 'Goodbye from my example modern npm package!';
-    return message;
+/**
+ * SequenceState for Cycle Control
+ * @remarks
+ * Descriptive flags to indicate when some event occurs in the hailstone sequences
+ * or tree graph reversal, when set to verbose, or stopping time check.
+ */
+export var SequenceState;
+(function (SequenceState) {
+    /** A Hailstone sequence state that indicates the stopping
+     *  time, a value less than the initial, has been reached. */
+    SequenceState["STOPPING_TIME"] = "STOPPING_TIME";
+    /** A Hailstone sequence state that indicates the total
+     *  stopping time, a value of 1, has been reached. */
+    SequenceState["TOTAL_STOPPING_TIME"] = "TOTAL_STOPPING_TIME";
+    /** A Hailstone and TreeGraph sequence state that indicates the
+     *  first occurence of a value that subsequently forms a cycle. */
+    SequenceState["CYCLE_INIT"] = "CYCLE_INIT";
+    /** A Hailstone and TreeGraph sequence state that indicates the
+     *  last occurence of a value that has already formed a cycle. */
+    SequenceState["CYCLE_LENGTH"] = "CYCLE_LENGTH";
+    /** A Hailstone and TreeGraph sequence state that indicates the sequence
+     *  or traversal has executed some imposed 'maximum' amount of times. */
+    SequenceState["MAX_STOP_OUT_OF_BOUNDS"] = "MAX_STOP_OUT_OF_BOUNDS";
+    /** A Hailstone sequence state that indicates the sequence terminated
+     *  by reaching "0", a special type of "stopping time". */
+    SequenceState["ZERO_STOP"] = "ZERO_STOP";
+})(SequenceState || (SequenceState = {}));
+/**
+ * Assert Sane Parameters
+ * @remarks
+ * Handles the sanity check for the parameterisation (P,a,b) required by both
+ * the function and reverse function.
+ * @param P - Modulus used to devide n, iff n is equivalent to (0 mod P).
+ * @param a - Factor by which to multiply n.
+ * @param b - Value to add to the scaled value of n.
+ * @throws FailedSaneParameterCheck
+ * Thrown if either P or a are 0.
+ */
+export function assertSaneParameterisation(P, a, b) {
+    // Sanity check (P,a,b) ~ P absolutely can't be 0. a "could" be zero
+    // theoretically, although would violate the reversability (if ~a is 0 then a
+    // value of "b" as the input to the reverse function would have a pre-emptive
+    // value of every number not divisible by P). The function doesn't _have_ to
+    // be reversable, but we are only interested in dealing with the class of
+    // functions that exhibit behaviour consistant with the collatz function. If
+    // _every_ input not divisable by P went straight to "b", it would simply
+    // cause a cycle consisting of "b" and every b/P^z that is an integer. While
+    // P in [-1, 1] could also be a reasonable check, as it makes every value
+    // either a 1 or 2 length cycle, it's not strictly an illegal operation.
+    // "b" being zero would cause behaviour not consistant with the collatz
+    // function, but would not violate the reversability, so no check either.
+    if (P === 0n) {
+        throw new FailedSaneParameterCheck(SaneParameterErrMsg.SANE_PARAMS_P);
+    }
+    if (a === 0n) {
+        throw new FailedSaneParameterCheck(SaneParameterErrMsg.SANE_PARAMS_A);
+    }
+}
+/**
+ * Parameterised Collatz Function
+ * @param n - The value on which to perform the Collatz-esque function
+ * @param P - Modulus used to devide n, iff n is equivalent to (0 mod P). Default is 2.
+ * @param a - Factor by which to multiply n. Default is 3.
+ * @param b - Value to add to the scaled value of n. Default is 1.
+ * @returns the output of a single application of a Collatz-esque function.
+ * @throws FailedSaneParameterCheck
+ * Thrown if either P or a are 0.
+ */
+export function Function({ n, P = 2n, a = 3n, b = 1n }) {
+    assertSaneParameterisation(P, a, b);
+    return n % P === 0n ? n / P : (a * n + b);
+}
+/**
+ * Parameterised Collatz Inverse Function
+ * @param n - The value on which to perform the reverse Collatz function
+ * @param P - Modulus used to devide n, iff n is equivalent to (0 mod P). Default is 2.
+ * @param a - Factor by which to multiply n. Default is 3.
+ * @param b - Value to add to the scaled value of n. Default is 1.
+ * @returns the output of a single application of a Collatz-esque reverse function.
+ * @throws FailedSaneParameterCheck
+ * Thrown if either P or a are 0.
+ */
+export function ReverseFunction({ n, P = 2n, a = 3n, b = 1n }) {
+    assertSaneParameterisation(P, a, b);
+    // Every input can be reversed as the result of "n/P" division, which yields
+    // "Pn"... {f(n) = an + b}≡{(f(n) - b)/a = n} ~ if n was such that the
+    // muliplication step was taken instead of the division by the modulus, then
+    // (f(n) - b)/a) must be an integer that is not in (0 mod P). Because we're
+    // not placing restrictions on the parameters yet, although there is a better
+    // way of shortcutting this for the default variables, we need to always
+    // attempt (f(n) - b)/a)
+    if ((n - b) % a === 0n && (n - b) % (P * a) !== 0n) {
+        return [(P * n), ((n - b) / a)];
+    }
+    else {
+        return [P * n];
+    }
 }
 export default {
-    helloWorld,
-    goodBye,
+    Function,
+    ReverseFunction,
 };
--- javascript/tests/index.spec.ts
+++ javascript/tests/index.spec.ts
@@ -1,43 +1,127 @@
-// This is an example test suite from
-// https://github.com/snyk-snippets/modern-npm-package/blob/main/tests/index.spec.ts
-// MIT: https://github.com/snyk-snippets/modern-npm-package/blob/main/LICENSE
+/* eslint-env mocha */
 
 import 'mocha';
 import { assert } from 'chai';
 
-import { helloWorld, goodBye } from '../src/index';
-import npmPackage from '../src/index';
+import * as Collatz from '../src/index';
+import defaultExport from '../src/index';
 
 describe('NPM Package', () => {
   it('should be an object', () => {
-    assert.isObject(npmPackage);
+    assert.isObject(defaultExport);
   });
 
-  it('should have a helloWorld property', () => {
-    assert.property(npmPackage, 'helloWorld');
-  });
-});
-
-describe('Hello World Function', () => {
-  it('should be a function', () => {
-    assert.isFunction(helloWorld);
+  it('should have a Function property', () => {
+    assert.property(defaultExport, 'Function');
   });
 
-  it('should return the hello world message', () => {
-    const expected = 'Hello World from my example modern npm package!';
-    const actual = helloWorld();
-    assert.equal(actual, expected);
+  it('should have a ReverseFunction property', () => {
+    assert.property(defaultExport, 'ReverseFunction');
   });
 });
 
-describe('Goodbye Function', () => {
+describe('Function', () => {
   it('should be a function', () => {
-    assert.isFunction(goodBye);
+    assert.isFunction(Collatz.Function);
   });
 
-  it('should return the goodbye message', () => {
-    const expected = 'Goodbye from my example modern npm package!';
-    const actual = goodBye();
+  // Default/Any (P,a,b); 0 trap
+  it('should return the Default/Any (P,a,b); 0 trap', () => {
+    const expected = 0n;
+    const actual = Collatz.Function({ n: 0n });
     assert.equal(actual, expected);
+  });
+
+  // Default/Any (P,a,b); 1 cycle; positives
+  it('should return the Default/Any (P,a,b); 1 cycle; positives', () => {
+    assert.equal(Collatz.Function({ n: 1n }), 4n);
+    assert.equal(Collatz.Function({ n: 4n }), 2n);
+    assert.equal(Collatz.Function({ n: 2n }), 1n);
+  });
+
+  // Default/Any (P,a,b); -1 cycle; negatives
+  it('should return the Default/Any (P,a,b); -1 cycle; negatives', () => {
+    assert.equal(Collatz.Function({ n: -1n }), -2n);
+    assert.equal(Collatz.Function({ n: -2n }), -1n);
+  });
+
+  // Test a wider modulo sweep by upping P to 5, a to 2, and b to 3
+  it('should return the Test a wider modulo sweep by upping P to 5, a to 2, and b to 3', () => {
+    assert.equal(Collatz.Function({ n: 1n, P: 5n, a: 2n, b: 3n }), 5n);
+    assert.equal(Collatz.Function({ n: 2n, P: 5n, a: 2n, b: 3n }), 7n);
+    assert.equal(Collatz.Function({ n: 3n, P: 5n, a: 2n, b: 3n }), 9n);
+    assert.equal(Collatz.Function({ n: 4n, P: 5n, a: 2n, b: 3n }), 11n);
+    assert.equal(Collatz.Function({ n: 5n, P: 5n, a: 2n, b: 3n }), 1n);
+  });
+
+  // Test negative P, a and b. We only use the (0 mod P) conjugacy class to
+  // determine functionality, so flooring negative P doesn't cause any issue.
+  it('should return the Test negative P, a and b.', () => {
+    assert.equal(Collatz.Function({ n: 1n, P: -3n, a: -2n, b: -5n }), -7n);
+    assert.equal(Collatz.Function({ n: 2n, P: -3n, a: -2n, b: -5n }), -9n);
+    assert.equal(Collatz.Function({ n: 3n, P: -3n, a: -2n, b: -5n }), -1n);
+  });
+
+  // Set P and a to 0 to assert on assertSaneParameterisation.
+  it('should return the assertion error', () => {
+    assert.throws(() => { Collatz.Function({ n: 1n, P: 0n, a: 2n, b: 3n }); }, Collatz.FailedSaneParameterCheck, Collatz.SaneParameterErrMsg.SANE_PARAMS_P);
+    assert.throws(() => { Collatz.Function({ n: 1n, P: 0n, a: 0n, b: 3n }); }, Collatz.FailedSaneParameterCheck, Collatz.SaneParameterErrMsg.SANE_PARAMS_P);
+    assert.throws(() => { Collatz.Function({ n: 1n, P: 1n, a: 0n, b: 3n }); }, Collatz.FailedSaneParameterCheck, Collatz.SaneParameterErrMsg.SANE_PARAMS_A);
+  });
+});
+
+describe('ReverseFunction', () => {
+  it('should be a function', () => {
+    assert.isFunction(Collatz.ReverseFunction);
+  });
+
+  // Default (P,a,b); 0 trap [as b is not a multiple of a]
+  it('should return the Default (P,a,b); 0 trap', () => {
+    const expected = [0n];
+    const actual = Collatz.ReverseFunction({ n: 0n });
+    assert.deepEqual(actual, expected);
+  });
+
+  // Default (P,a,b); 1 cycle; positives
+  it('should return the Default (P,a,b); 1 cycle; positives', () => {
+    assert.deepEqual(Collatz.ReverseFunction({ n: 1n }), [2n]);
+    assert.deepEqual(Collatz.ReverseFunction({ n: 4n }), [8n, 1n]);
+    assert.deepEqual(Collatz.ReverseFunction({ n: 2n }), [4n]);
+  });
+
+  // Default (P,a,b); -1 cycle; negatives
+  it('should return the Default (P,a,b); -1 cycle; negatives', () => {
+    assert.deepEqual(Collatz.ReverseFunction({ n: -1n }), [-2n]);
+    assert.deepEqual(Collatz.ReverseFunction({ n: -2n }), [-4n, -1n]);
+  });
+
+  // Test a wider modulo sweep by upping P to 5, a to 2, and b to 3
+  it('should return the Test a wider modulo sweep by upping P to 5, a to 2, and b to 3', () => {
+    assert.deepEqual(Collatz.ReverseFunction({ n: 1n, P: 5n, a: 2n, b: 3n }), [5n, -1n]);
+    assert.deepEqual(Collatz.ReverseFunction({ n: 2n, P: 5n, a: 2n, b: 3n }), [10n]);
+    assert.deepEqual(Collatz.ReverseFunction({ n: 3n, P: 5n, a: 2n, b: 3n }), [15n]); // also tests !0
+    assert.deepEqual(Collatz.ReverseFunction({ n: 4n, P: 5n, a: 2n, b: 3n }), [20n]);
+    assert.deepEqual(Collatz.ReverseFunction({ n: 5n, P: 5n, a: 2n, b: 3n }), [25n, 1n]);
+  });
+
+  // Test negative P, a and b. We only use the (0 mod P) conjugacy class to
+  // determine functionality, so flooring negative P doesn't cause any issue.
+  it('should return the Test negative P, a and b.', () => {
+    assert.deepEqual(Collatz.ReverseFunction({ n: 1n, P: -3n, a: -2n, b: -5n }), [-3n]); // != [-3, -3]
+    assert.deepEqual(Collatz.ReverseFunction({ n: 2n, P: -3n, a: -2n, b: -5n }), [-6n]);
+    assert.deepEqual(Collatz.ReverseFunction({ n: 3n, P: -3n, a: -2n, b: -5n }), [-9n, -4n]);
+  });
+
+  // Set P and a to 0 to assert on assertSaneParameterisation
+  it('should return the assertion error', () => {
+    assert.throws(() => { Collatz.ReverseFunction({ n: 1n, P: 0n, a: 2n, b: 3n }); }, Collatz.FailedSaneParameterCheck, Collatz.SaneParameterErrMsg.SANE_PARAMS_P);
+    assert.throws(() => { Collatz.ReverseFunction({ n: 1n, P: 0n, a: 0n, b: 3n }); }, Collatz.FailedSaneParameterCheck, Collatz.SaneParameterErrMsg.SANE_PARAMS_P);
+    assert.throws(() => { Collatz.ReverseFunction({ n: 1n, P: 1n, a: 0n, b: 3n }); }, Collatz.FailedSaneParameterCheck, Collatz.SaneParameterErrMsg.SANE_PARAMS_A);
+  });
+
+  // If b is a multiple of a, but not of Pa, then 0 can have a reverse.
+  it('should return the If b is a multiple of a, but not of Pa, then 0 can have a reverse.', () => {
+    assert.deepEqual(Collatz.ReverseFunction({ n: 0n, P: 17n, a: 2n, b: -6n }), [0n, 3n]);
+    assert.deepEqual(Collatz.ReverseFunction({ n: 0n, P: 17n, a: 2n, b: 102n }), [0n]);
   });
 });
